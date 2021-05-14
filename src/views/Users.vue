7b06ad3e7b37f0bf0fdaef0e36e9c786a0ed8b65
promin3737 <s.s.hayabusa3737@icloud.com>
2021-05-14 09:58:18 +0900

    router-linkの使い方

---
+++ src/views/Users.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>Users</h3>
+    <router-link to="/users/1">ユーザー１</router-link>
+    <router-link to="/users/2">ユーザー2</router-link>
+    <hr />
+    <h1>User No. {{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id"]
+};
+</script>
